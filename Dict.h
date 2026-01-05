c6acee6d917aa9d6382940d5698c167eb63e5a96
epsamtoe <epsamotoe@gmail.com>
2026-01-05 20:29:44 +0100

    3

--- Dict.h
+++ Dict.h
@@ -7,18 +7,17 @@
 template <typename V>
 class Dict {
     public:
-        // Destructor virtual (importante para interfaces)
-        virtual ~Dict() {}
 
-        // Métodos virtuales puros (= 0)
+        // Destructor
+        virtual ~Dict() {}
         
-        // Inserta el par key->value. Lanza error si ya existe.
+        // Inserta el par key->value. 
         virtual void insert(std::string key, V value) = 0;
 
-        // Busca el valor asociado a key. Lanza error si no lo encuentra.
+        // Busca el valor asociado a key.
         virtual V search(std::string key) = 0;
 
-        // Elimina el par asociado a key. Lanza error si no lo encuentra.
+        // Elimina el par asociado a key.
         virtual V remove(std::string key) = 0;
 
         // Devuelve el número de elementos
